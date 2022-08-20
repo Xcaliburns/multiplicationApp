6602c8c90377b2cfe03e8b2a7493dead7bc4f5f7
Xcaliburns <64249039+Xcaliburns@users.noreply.github.com>
2022-08-20 18:30:26 +0200

    Add files via upload
    projet terminer mais penser à faire un bouton reset

---
+++ index.js
@@ -0,0 +1,47 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const questionEl = document.getElementById("question");
+
+const inputEl = document.getElementById("input");
+
+const formEl = document.getElementById("form");
+
+const justeEl = document.getElementById("juste");
+const fauxEl = document.getElementById("faux");
+
+let juste = JSON.parse(localStorage.getItem("juste"));
+
+if (!juste) {
+    juste = 0;
+}
+
+justeEl.innerText = `juste: ${juste}`;
+
+let faux = JSON.parse(localStorage.getItem("faux"));
+
+if (!faux) {
+    faux = 0;
+}
+
+fauxEl.innerText = `faux: ${faux}`;
+
+questionEl.innerText = `Combien font ${num1} X ${num2}?`;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+    const userAns = +inputEl.value;
+    if (userAns === correctAns) {
+        juste++;
+        updateLocalStorage();
+    } else {
+        faux++;
+        updateLocalStorage();
+    }
+});
+
+function updateLocalStorage() {
+    localStorage.setItem("juste", JSON.stringify(juste));
+    localStorage.setItem("faux", JSON.stringify(faux));
+}
